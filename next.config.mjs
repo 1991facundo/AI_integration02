e74fb3f0c45d34335ed177567a8486cd22e9b113
Facundo Fernandez <99399998+1991facundo@users.noreply.github.com>
2024-07-15 16:50:42 +0200

    commit test debug

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import 'dotenv/config';
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_OPENAI_API_KEY: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+  },
+};
 
 export default nextConfig;
